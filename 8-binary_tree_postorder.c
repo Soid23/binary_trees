d0d9f3494b4055e427efb5770f2b47d4fd6f447c
Soid23 <dorrismutahi@gmail.com>
2024-02-27 12:04:40 -0800

    postorder

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - post-order traversal.
+ * @tree: points node.
+ * @func: calls each node.
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_postorder(tree->left func);
+		binary_tree_postorde(tree->right func);
+		func(tree->n);
+	}
+}
