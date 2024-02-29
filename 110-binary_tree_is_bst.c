60245061629fcdbf978983c7b3288bef78953618
Soid23 <dorrismutahi@gmail.com>
2024-02-29 03:23:32 -0800

    bst

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_bst - checks if a binary tree is a valid.
+ * @tree: pointer.
+ * Return: if tree null (0).
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	int var = 0, left = 2, right = 2;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left && tree->left->n > tree->n)
+		return (0);
+	if (tree->right && tree->right->n < tree->n)
+		return (0);
+	if (tree->left && tree->left->n < tree->n)
+	{
+		var = check_sub_tree_Left(tree->left, tree->n);
+		if (var == 0)
+			return (0);
+		left = binary_tree_is_bst(tree->left);
+	}
+	if (tree->right && tree->right->n > tree->n)
+	{
+		var = check_sub_tree_Right(tree->right, tree->n);
+		if (var == 0)
+			return (0);
+		right = binary_tree_is_bst(tree->right);
+	}
+	if (left != 2 || right != 2)
+	{
+		if (left == 0 || right == 0)
+			return (0);
+	}
+	return (1);
+}
