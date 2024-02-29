05c55172e81059e3a27b5ac676929eed7daac342
Soid23 <dorrismutahi@gmail.com>
2024-02-28 19:40:29 -0800

    left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,21 +7,21 @@
 */  
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new = malloc(sizeof(binary_tree_t));
-    if (!new)
-		return (new);
+	binary_tree_t *new;
 
-	if (!parent)
-	{
-		free(new);
+	if (parent == NULL)
 		return (NULL);
+
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
 	}
-	new->left = parent->left;
-	new->parent = parent;
-	new->right = NULL;
-	new->n = value;
 	parent->left = new;
 
 	return (new);
-
 }
