c61afe8d81f47cff3dd0f044b11bfa413bf58088
Soid23 <dorrismutahi@gmail.com>
2024-02-28 20:58:07 -0800

    change

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -19,13 +19,13 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        if (tree)
-        {
-                size_t lef = 0, righ = 0;
+	if (tree)
+	{
+		size_t lef = 0, righ = 0;
 
-                lef = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-                righ = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-                return ((lef > righ) ? lef : righ);
-        }
-        return (0);
+		lef = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		righ = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((lef > righ) ? lef : righ);
+	}
+	return (0);
 }
