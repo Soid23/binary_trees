1bf31fb4f26f6e1dfd7c3d078069287e6f4c7cb2
Soid23 <dorrismutahi@gmail.com>
2024-02-28 21:06:59 -0800

    add

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -8,10 +8,13 @@
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
+	{
 		return;
-
-		func(tree->root);
+	}
+	else
+	{
+		func(tree->n);
 		binary_tree_preorder(tree->left, func);
 		binary_tree_preorder(tree->right, func);
-
+	}
 }
